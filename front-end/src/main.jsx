aaf932f08898db9fd56557db8fef1902c3b13727
Nebiyu Main <107809704+nebiyuu1954@users.noreply.github.com>
2025-10-03 18:00:02 +0300

    added i18n

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import "./il8n";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  ,
 )
